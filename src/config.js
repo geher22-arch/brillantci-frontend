95950fc085f5bf67bbc2a1a1c02aac6057b17cca
Hermann Essoh <geher22@gmail.com>
2026-04-22 10:01:52 +0000

    Fix API URL for production

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-const API = process.env.REACT_APP_API_URL || "http://localhost:3000";
+const API = process.env.REACT_APP_API_URL
+  || (window.location.hostname === 'localhost' ? "http://localhost:3000" : "https://brillantci-api.onrender.com");
 export default API;
